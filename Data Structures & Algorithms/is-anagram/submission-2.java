d083416576a292f5aaaba79c42bbf2c026f411f0
Aloysius Q. Gonzaga <gonzaga.aloysius42@gmail.com>
2026-07-09 13:16:01 +0800

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+
+        Set<Character> hash1 = new HashSet<>();
+        Set<Character> hash2 = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++){
+            hash1.add(s.charAt(i));
+        }
+        for (int i = 0; i < t.length(); i++){
+            hash2.add(t.charAt(i));
+        }
+
+            if(hash1.containsAll(hash2) && hash2.containsAll(hash1)){
+                return true;
+            }
+
+            return false;
+        
+
+    }
+}
